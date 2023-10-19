20e0636d0f7a31b90b634119571941c1e9888d1c
jardimdanificado <jardeltorreslima01@gmail.com>
2023-10-19 04:33:44 -0300

    0.4.56 minor change

--- example/server/server.mjs
+++ example/server/server.mjs
@@ -2,4 +2,4 @@ import * as saposocket from '../../saposocket.mjs'
 
 let server = new saposocket.Server();
 server.plugin(await import('../../plugin/file.mjs'));
-server.plugin(await import('../../plugin/lua.mjs'));
+server.plugin(await import('../../plugin/lua.mjs')); //exemplo: luaevalfile ../../plugin/lua_example/factorial.lua
