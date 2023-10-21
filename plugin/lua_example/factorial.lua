854c0702d08a670c653d236398ae4e12f3fe132c
jardimdanificado <jardeltorreslima01@gmail.com>
2023-10-21 03:15:35 -0300

    0.4.6 lua-interop moved to its own repo

--- plugin/lua_example/factorial.lua
+++
@@ -1,8 +0,0 @@
---não pode ter primeira linha em branco
-function fatorial(n) if n == 0 then return 1 else return n * fatorial(n - 1) end end;
-
-numero = 5;
-resultado = fatorial(numero);
-json("O fatorial de " .. numero .. " é " .. resultado);
---funções tem que estar em uma linha apenas
---nao pode variaveis locais
